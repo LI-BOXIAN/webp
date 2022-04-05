104b1798aee58181a7a552c51a052970d019247e
LI-BOXIAN <100269315+LI-BOXIAN@users.noreply.github.com>
2022-04-05 23:58:44 +0800

    Update HW3.js

--- HW3/HW3.js
+++ HW3/HW3.js
@@ -3,19 +3,19 @@ document.addEventListener("keydown",disappear);
 document.addEventListener("keydown",settime);
 for(var i=0;i<17;i++){
 document.getElementById("AAA").innerHTML +=getRandomChar();
-}/*初始賽道*/
+}
 function insertStr(soure,start, newStr){
   return soure.slice(0, start) + newStr + soure.slice(start)
-}/*從左插入*/
+}
 function getRandomChar() {
     var r = Math.floor(Math.random() * 26);
     return String.fromCharCode(65 + r);
-}/*隨機字母*/
+}
 function produce(){
  var x=getRandomChar(); 
  var plus=document.getElementById("AAA").innerText
 document.getElementById("AAA").innerHTML = insertStr(plus,6,x);
-}/*固定製造*/
+}
 function disappear(e) {
   var keyCode = (window.event) ? e.which : e.keyCode;
    var del=document.getElementById("AAA").innerText;
@@ -23,7 +23,7 @@ var n = String.fromCharCode(keyCode);
   if(n==del.substring(del.length-1, del.length)){
 document.getElementById("AAA").innerHTML=del.substring(0, del.length - 1);
     }
-}/*從右消失*/
+}
 var lastInputTime = null;
 function settime(){
 var today = new Date().getTime();
@@ -35,4 +35,4 @@ var today = new Date().getTime();
     }
 }
 lastInputTime=today;
-}/*2.5word/sec*/
+}

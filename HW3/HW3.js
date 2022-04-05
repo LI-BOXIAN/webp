4f41d15856b92c9382584d9bbfeea48874298b8e
LI-BOXIAN <100269315+LI-BOXIAN@users.noreply.github.com>
2022-04-05 12:07:55 +0800

    Update HW3.js

--- HW3/HW3.js
+++ HW3/HW3.js
@@ -2,7 +2,7 @@ document.addEventListener("keydown",produce);
 document.addEventListener("keydown",disappear);
 document.addEventListener("keydown",settime);
 for(var i=0;i<17;i++){
-document.getElementById("constant").innerHTML +=getRandomChar();
+document.getElementById("AAA").innerHTML +=getRandomChar();
 }/*初始賽道*/
 function insertStr(soure,start, newStr){
   return soure.slice(0, start) + newStr + soure.slice(start)
@@ -13,15 +13,15 @@ function getRandomChar() {
 }/*隨機字母*/
 function produce(){
  var x=getRandomChar(); 
- var plus=document.getElementById("constant").innerText
-document.getElementById("constant").innerHTML = insertStr(plus,6,x);
+ var plus=document.getElementById("AAA").innerText
+document.getElementById("AAA").innerHTML = insertStr(plus,6,x);
 }/*固定製造*/
 function disappear(e) {
   var keyCode = (window.event) ? e.which : e.keyCode;
-   var del=document.getElementById("constant").innerText;
+   var del=document.getElementById("AAA").innerText;
 var n = String.fromCharCode(keyCode);
   if(n==del.substring(del.length-1, del.length)){
-document.getElementById("constant").innerHTML=del.substring(0, del.length - 1);
+document.getElementById("AAA").innerHTML=del.substring(0, del.length - 1);
     }
 }/*從右消失*/
 var lastInputTime = null;

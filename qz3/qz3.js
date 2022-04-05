df62a9fe651bd6f1e3a258a6c35395427a285e6b
LI-BOXIAN <100269315+LI-BOXIAN@users.noreply.github.com>
2022-04-05 12:06:09 +0800

    Update qz3.js

--- qz3/qz3.js
+++ qz3/qz3.js
@@ -1,6 +1,6 @@
 document.addEventListener("keydown",logkey);
 function logkey(e) {
 var key=e.key
-var p=document.getElementById("constant");
+var p=document.getElementById("123");
 p.innerHTML+= key;
 }
